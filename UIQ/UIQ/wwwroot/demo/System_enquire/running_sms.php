aec8c93b024654006c82c4505bb15dcd1567f1ad
FrancisHuang63 <noct.versus@gmail.com>
2022-10-03 23:23:53 +0800

    移除demo 目錄

--- UIQ/UIQ/wwwroot/demo/System_enquire/running_sms.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../function.php");
-echo "<pre>";
-$command="../shell/sms_enquire.ksh {$_POST['p_model']} {$_POST['p_member']} | grep -v 'Goodbye \|Welcome \|logged \|logout'";
-$result=shell_exec($command);
-sleep(1.5);
-echo $result;
-?>
